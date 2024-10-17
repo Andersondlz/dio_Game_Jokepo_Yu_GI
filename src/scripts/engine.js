005e6dc5ec0b8620c556955fab8f257a433926e7
Andersondlz <andersondlz2009@gmail.com>
2024-10-16 22:31:55 -0300

    commit engine.js

---
+++ src/scripts/engine.js
@@ -0,0 +1,24 @@
+const state = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    actions:{
+        button: document.getElementById("next-duel"),
+    },
+}
+
+
+
+
+function init(){
+
+}
+
+init();
