9bf0b293c8992743fc100ae77b0e76da5addb80e
Andersondlz <andersondlz2009@gmail.com>
2024-10-19 10:37:34 -0300

    commit criando a logica das cartas e selecionando elas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -14,11 +14,81 @@ const state = {
     },
 }
 
+const playerSides = {
+    player1: "player-cards",
+    computer: "computer-cards",
+}
 
 
+const pathImages = "./src/assets/icons/";
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img:`${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img:`${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id:2,
+        name: "Exodia",
+        type: "Scissord",
+        img:`${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    }
+]
+
+
+async function getRandomIdCard(){
+    const randomIdCard = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIdCard].id;
+}
+
+
+async function createCardImage(IdCard, fildSide){
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src","./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if(fildSide === playerSides.player1){
+        cardImage.addEventListener("click", () => {
+            setCardsFiel(cardImage.getAttribute("data-id"));
+        })
+    }
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(IdCard)
+    })
+    return cardImage;
+}
+
+
+
+async function drawCards(cardNumber, fildSide){
+    for(let i = 0; i < cardNumber; i++){
+        const randomIdCard = await getRandomIdCard();
+        const cardImage = await createCardImage(randomIdCard, fildSide);
+        document.getElementById(fildSide).appendChild(cardImage);
+
+    }
+}  
+    
 
 function init(){
-
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
